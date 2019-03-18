3acd9c02095b5dcf93ab17bb5fe1ac743136bdec
Eric Sink <eric@sourcegear.com>
2019-03-18 09:23:52 -0500

    fix to run through the whole src directory

--- f/Program.fs
+++ f/Program.fs
@@ -86,22 +86,35 @@ let write_with_front_matter (dest : string) (d: Dictionary<string,string>) (s :s
 let do_file f =
     let src = File.ReadAllText(f)
     let (front_matter, html) = get_front_matter src
-    let parser = HtmlParser()
-    parser.Error.Add(
-        fun s -> 
-            let e = s :?> HtmlErrorEvent
-            printfn "%d: %s" e.Code e.Message
-        )
-    let dom = parser.ParseDocument("<html><body></body></html>")
-    let nodes = parser.ParseFragment(html, dom.Body)
-    let sw = StringWriter()
-    let fmt = PrettyMarkupFormatter()
-    for n in nodes do
-        n.ToHtml(sw, fmt)
-    let new_html = sw.ToString()
-    write_with_front_matter f front_matter new_html
+    if front_matter <> null then
+        let parser = HtmlParser()
+        parser.Error.Add(
+            fun s -> 
+                let e = s :?> HtmlErrorEvent
+                printfn "%d: %s" e.Code e.Message
+            )
+        let dom = parser.ParseDocument("<html><body></body></html>")
+        let nodes = parser.ParseFragment(html, dom.Body)
+        let sw = new StringWriter()
+        let fmt = PrettyMarkupFormatter()
+        for n in nodes do
+            n.ToHtml(sw, fmt)
+        let new_html = sw.ToString()
+        write_with_front_matter f front_matter new_html
     
+let rec do_dir (from :string) =
+    for f in (Directory.GetFiles(from)) do
+        do_file f
+
+    for from_sub in (Directory.GetDirectories(from)) do
+        let name = Path.GetFileName(from_sub)
+        // TODO skip _layouts at every depth, or just at the top?
+        if name <> "_layouts" then
+            do_dir from_sub
+
 [<EntryPoint>]
 let main argv =
-    do_file argv.[0]
+    let dir_src = argv.[0]
+    do_dir dir_src
     0 // return an integer exit code
+
